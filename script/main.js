7df5efdc5d7b351e7eab09668d6e5cb27655bf0f
dianaki <diana.kuramshina21@gmail.com>
2021-11-03 20:29:56 +0300

    complete adaptive and swiper

---
+++ script/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider__button--next',
+      prevEl: '.slider__button--prev',
+    },
+  });
